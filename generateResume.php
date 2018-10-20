7438a5bfc686fbba4cc5d490b6fd9a1160f7ba9b
Azymic <amylu2009@gmail.com>
2018-10-20 13:36:07 -0500

    created

---
+++ generateResume.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
