15faa1d37d60103cb594373f1c82a6922505f4ec
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-01-13 22:30:59 -0500

    Deleted old; imported html

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/HomePage';
-import HireMePwease from './pages/HireMePwease';
-import Website from './pages/Website';
-import FowlPlay from './pages/FowlPlay';
-import Peekapak from './pages/Peekapak';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path='/' component={ Home } />
-        <Route path='/Peekapak' component={ Peekapak } />
-        <Route path='/HireMePwease' component={ HireMePwease } />
-        <Route path='/Website' component={ Website } />
-        <Route path='/FowlPlay' component={ FowlPlay } />
       </div>
     );
   }
