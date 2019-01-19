5fbf28f56b1a368ac0f8f88640540a6a450953c1
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-01-19 01:21:18 -0500

    Generates resume with App object

--- src/App.js
+++ src/App.js
@@ -4,11 +4,48 @@ import Home from './pages/HomePage';
 import Resume from './pages/ResumePage';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state={
+			name: "Name_",
+			hobbies: [
+				{
+					title: "Htitle",
+					description: "Hdescription",
+				}
+			],
+			projects: [
+				{
+					title: "Ptitle",
+					description: "Pdescription",
+				}
+			],
+			relevantWork: [
+				{
+					title: "Rtitle",
+					description: "Rdescription",
+				}
+			],
+			languages: [
+				{
+					title: "Ltitle",
+					description: "Ldescription",
+				}
+			],
+			contactInfo: {
+				phone: "420-666-6969",
+				caption: "Tinder me up_",
+			}
+
+		}
+	}
+
   render() {
     return (
       <div>
         <Route exact path='/' component={ Home } />
-        <Route exact path='/resume' component={ Resume } />
+        <Route path='/resume' render={(props) => <Resume state={this.state} {...props} />} />
       </div>
     );
   }
