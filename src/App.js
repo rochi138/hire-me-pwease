098e176b4b559e8ce02fb38753dc793c051f7ff6
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-01-19 00:19:04 -0500

    Added resume page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './pages/HomePage';
+import Resume from './pages/ResumePage';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path='/' component={ Home } />
+        <Route exact path='/resume' component={ Resume } />
       </div>
     );
   }
