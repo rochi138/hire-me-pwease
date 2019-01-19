45aa8001e7c0be5d63bd92aeeff3da3909bbae37
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-01-19 18:53:32 -0500

    Removed objectUnderEdit; Resume displays state

--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -17,7 +17,7 @@ export default class Resume extends Component{
   }
 
   render(){
-    var source = this.props.state.objectUnderEdit;
+    var source = this.props.state;
     return(
       <div>
         <div id="resume" className={ styles.resume }>
@@ -25,40 +25,67 @@ export default class Resume extends Component{
                 <h1>Name</h1>
                 <div>
                     { source.name.firstName }
+                    { source.name.lastName }
                 </div>
             </div>
             <div id="Page" className={ styles.page }>
                 <div className={ styles.left }>
                     <div id="Hobbies" style={{borderBottom: "3px solid #000000"}}>
                         <h1>Hobbies</h1>
-                        <div>
-                            Anime
-                        </div>
+                        { source.hobbies.map( ( hobby, i ) =>
+                            <div key = { i }>
+                                <div>
+                                  { hobby.title }
+                                </div>
+                                <div>
+                                  { hobby.description }
+                                </div>
+                            </div>
+                        ) }
                     </div>
                     <div id="Projects" style={{borderBottom: "3px solid #000000"}}>
                         <h1>Projects</h1>
-                        <div>
-                            FowlPlay
-                        </div>
+                        { source.projects.map( ( project, i ) =>
+                            <div key = { i }>
+                                <div>
+                                  { project.title }
+                                </div>
+                                <div>
+                                  { project.description }
+                                </div>
+                            </div>
+                        ) }
                     </div>
                     <div id="Relevant Work" style={{borderBottom: "3px solid #000000"}}>
                         <h1>Relevant Work</h1>
-                        <div>
-                            Butthole Chocolate Manufacteurer
-                        </div>
+                        { source.relevantWork.map( ( relevantWork, i ) =>
+                            <div key = { i }>
+                                <div>
+                                  { relevantWork.title }
+                                </div>
+                                <div>
+                                  { relevantWork.description }
+                                </div>
+                            </div>
+                        ) }
                     </div>
                 </div>
                 <div className={ styles.right }>
                     <div id="Languages">
                         <h1>Languages</h1>
-                        <div>
-                            Klingon
-                        </div>
+                        { source.languages.map( ( language, i ) =>
+                            <div key = { i }>
+                              { language }
+                            </div>
+                        ) }
                     </div>
                     <div id="Contact Info">
                         <h1>Contact Info</h1>
                         <div>
-                            Tinder me up
+                            { source.contactInfo.phone }
+                        </div>
+                        <div>
+                            { source.contactInfo.caption }
                         </div>
                     </div>
                 </div>
