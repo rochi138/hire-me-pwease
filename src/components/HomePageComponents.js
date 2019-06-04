3dd49ff09bd90f450641812fbddb0083992483cb
rochi138 <rjching@uwaterloo.ca>
2019-06-04 14:46:16 -0400

    Bootstrap form; all/lucky checkboxes

--- src/components/HomePageComponents.js
+++ src/components/HomePageComponents.js
@@ -15,7 +15,6 @@ export class CheckBoxesComponent extends Component {
 		this.state={
 			source: source,
 			checkStates: checkStates,
-			allTheText: this.props.allTheText,
 			title: this.props.title,
 		}
 	}
@@ -24,22 +23,50 @@ export class CheckBoxesComponent extends Component {
     var checkStates = this.state.checkStates;
     checkStates[key] = !checkStates[key];
     this.props.update( checkStates );
-  }
+	}
+	
+	selectAll() {
+		var checkStates = this.state.checkStates;
+		for (var i = 0; i < checkStates.length; i++) { 
+			checkStates[i] = true;
+		}
+		this.props.update( checkStates );
+	}
+
+	lucky() {
+		var checkStates = this.state.checkStates;
+		const amount = Math.floor(Math.random() * ( checkStates.length ));
+		console.log(amount);
+		for (var i = 0; i < amount; i++) {
+			var rand = Math.floor(Math.random() * ( checkStates.length ));
+			console.log(rand);
+			checkStates[ rand ] = !checkStates[ rand ];
+		}
+		this.props.update( checkStates );
+	}
 
 	render() {
 		return (
 			<div className={ styles.checkboxesComponent }>
 				<h2>{ this.state.title }</h2>
-				{ this.state.source.map( ( component, i ) =>
-            <div class="form-check form-check-inline" key={ i }>
-                <input class="form-check-input" type="checkbox" onClick={() => this.update( i )}/>
-                <label class="form-check-label">{component.title}</label>
-            </div>
-        ) }
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="checkboxHobbyLucky" value="Lucky" />
-            <label class="form-check-label" for="checkboxHobbyLucky">{ this.state.allTheText }</label>
-        </div>
+				<div class="form-group row">
+					{ this.state.source.map( ( component, i ) =>
+							<div class="custom-control custom-checkbox col-4" key={ i } onClick={() => this.update( i )}>
+								<input type="checkbox" class="custom-control-input" checked={ (this.state.checkStates[ i ]) ? "checked" : "" }/> 
+								<label class="custom-control-label">{ component.title }</label>
+							</div>
+					) }
+				</div>
+				<div class="form-group row">
+					<div class="custom-control custom-checkbox col-4" onClick={() => this.selectAll()}>
+						<input type="checkbox" class="custom-control-input"/> 
+						<label class="custom-control-label">All the { this.state.title }!</label>
+					</div>
+					<div class="custom-control custom-checkbox col-4" onClick={() => this.lucky()}>
+						<input type="checkbox" class="custom-control-input"/> 
+						<label class="custom-control-label">I'm feeling lucky ;)</label>
+					</div>
+				</div>
 			</div>
 		)
 	}
