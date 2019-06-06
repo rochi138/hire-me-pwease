b63d6664a40d79529f833a747559a3befe521aed
rochi138 <rjching@uwaterloo.ca>
2019-06-06 13:25:17 -0400

    PDF downlaods right size

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,144 +8,144 @@ import { CheckBoxesComponent } from '../components/HomePageComponents';
 import { Form } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
-  faPuzzlePiece,
-  faWrench,
-  faUserTie,
-  faEnvelope,
-  faPhone,
-  faFireAlt,
-  faHashtag,
-  faLanguage
+    faPuzzlePiece,
+    faWrench,
+    faUserTie,
+    faEnvelope,
+    faPhone,
+    faFireAlt,
+    faHashtag,
+    faLanguage
  } from '@fortawesome/free-solid-svg-icons'
 
 export default class Home extends Component{
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.updateHobbies = this.updateHobbies.bind( this );
-    this.updateProjects = this.updateProjects.bind( this );
-    this.updateRelevantWork = this.updateRelevantWork.bind( this );
-    this.updateLanguages = this.updateLanguages.bind( this );
-  }
+        this.updateHobbies = this.updateHobbies.bind( this );
+        this.updateProjects = this.updateProjects.bind( this );
+        this.updateRelevantWork = this.updateRelevantWork.bind( this );
+        this.updateLanguages = this.updateLanguages.bind( this );
+    }
 
-  generateResume() {
-    this.props.history.push( '/resume' );
-  }
+    generateResume() {
+        this.props.history.push( '/resume' );
+    }
 
-  updateFields( updatedFields ) {
-    this.props.moveStateUp( { ...this.props.state, ...updatedFields } );
-  }
+    updateFields( updatedFields ) {
+        this.props.moveStateUp( { ...this.props.state, ...updatedFields } );
+    }
 
-  updateName( updatedName ) {
-    this.updateFields( { name: { ...this.props.state.name, ...updatedName } } );
-  }
+    updateName( updatedName ) {
+        this.updateFields( { name: { ...this.props.state.name, ...updatedName } } );
+    }
 
-  updateContactInfo( updatedContactInfo ) {
-    this.updateFields( {contactInfo: {...this.props.state.contactInfo, ...updatedContactInfo } } );
-  }
+    updateContactInfo( updatedContactInfo ) {
+        this.updateFields( {contactInfo: {...this.props.state.contactInfo, ...updatedContactInfo } } );
+    }
 
-  updateHobbies( updateHobbies ) {
-    this.updateFields( { hobbies: updateHobbies } );
-  }
+    updateHobbies( updateHobbies ) {
+        this.updateFields( { hobbies: updateHobbies } );
+    }
 
-  updateProjects( updatedProjects ) {
-    this.updateFields( { projects: updatedProjects } );
-  }
+    updateProjects( updatedProjects ) {
+        this.updateFields( { projects: updatedProjects } );
+    }
 
-  updateRelevantWork( updatedRelevantWork ) {
-    this.updateFields( { relevantWork: updatedRelevantWork } );
-  }
+    updateRelevantWork( updatedRelevantWork ) {
+        this.updateFields( { relevantWork: updatedRelevantWork } );
+    }
 
-  updateLanguages( updatedLanguages ) {
-    this.updateFields( { languages: updatedLanguages } );
-  }
+    updateLanguages( updatedLanguages ) {
+        this.updateFields( { languages: updatedLanguages } );
+    }
 
-  render(){
-    return(
-      <div>
-        <div className="container">
-          <div style={{ display: "inline-block", backgroundColor: "white", paddingRight: "15px"}}>
-            <h1>Hire Me Pwease :3c</h1>
-            <p>Help me Mr. Obama.</p>
-          </div>
-        </div>
-        <div className="container">
-          <Form>
-            <div style={{ display: "inline-block", backgroundColor: "white"}}>
-              <h2>Personal Information</h2>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <div className="form-group">
-                        <label>First Name</label>
-                        <input className="form-control" placeholder="Chad, Stacey, Elon, Obama" onChange={(e) => this.updateName({firstName: e.target.value })} />
+    render(){
+        return(
+            <div>
+                <div className="container">
+                    <div style={{ display: "inline-block", backgroundColor: "white", paddingRight: "15px"}}>
+                        <h1>Hire Me Pwease :3c</h1>
+                        <p>Help me Mr. Obama.</p>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="form-group">
-                        <label >Last Name</label>
-                        <input className="form-control" placeholder="Smith, Musk, Gates, Jobs, etc." onChange={(e) => this.updateName({lastName: e.target.value })}/>
-                    </div>
+                <div className="container">
+                    <Form>
+                        <div style={{ display: "inline-block", backgroundColor: "white"}}>
+                            <h2>Personal Information</h2>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="form-group">
+                                    <label>First Name</label>
+                                    <input className="form-control" placeholder="Chad, Stacey, Elon, Obama" onChange={(e) => this.updateName({firstName: e.target.value })} />
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="form-group">
+                                    <label >Last Name</label>
+                                    <input className="form-control" placeholder="Smith, Musk, Gates, Jobs, etc." onChange={(e) => this.updateName({lastName: e.target.value })}/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="contact-information">
+                            <div className="row">
+                                <div className="col">
+                                    <FontAwesomeIcon icon={ faEnvelope } style={{ marginRight: "5px"}}/>
+                                    <label>Email Address</label>
+                                    <input className="form-control" placeholder="president@uwaterloo.ca" onChange={(e) => this.updateContactInfo({email: e.target.value })} />
+                                </div>
+                                <div className="col">
+                                    <FontAwesomeIcon icon={ faPhone } style={{ marginRight: "5px"}}/>
+                                    <label>Phone Number</label>
+                                    <input className="form-control" placeholder="666-420-6969" onChange={(e) => this.updateContactInfo({phone: e.target.value })} />
+                                </div>
+                                <div className="col">
+                                    <FontAwesomeIcon icon={ faFireAlt } style={{ marginRight: "5px"}}/>
+                                    <label>Tinder Profile</label>
+                                    <input className="form-control" placeholder="looking4truluv" onChange={(e) => this.updateContactInfo({tinder: e.target.value })} />
+                                </div>
+                                <div className="col">
+                                    <FontAwesomeIcon icon={ faHashtag } style={{ marginRight: "5px"}}/>
+                                    <label>Instagram</label>
+                                    <input className="form-control" placeholder="@BarackObama" onChange={(e) => this.updateContactInfo({instagram: e.target.value })} />
+                                </div>
+                            </div>
+                        </div>
+                        <CheckBoxesComponent 
+                            title="Languages"
+                            fileName="Languages"
+                            componentName="languages"
+                            icon={ faLanguage }
+                            update={ this.updateLanguages }
+                        />
+                        <CheckBoxesComponent 
+                            title="Projects"
+                            fileName="Projects"
+                            componentName="projects"
+                            icon={ faWrench }
+                            update={ this.updateProjects }
+                        />
+                        <CheckBoxesComponent 
+                            title="Relevant Work"
+                            fileName="RelevantWork"
+                            componentName="relevantWork"
+                            icon={ faUserTie }
+                            update={ this.updateRelevantWork }
+                        />
+                        <CheckBoxesComponent 
+                            title="Hobbies"
+                            fileName="Hobbies"
+                            componentName="hobbies"
+                            icon={ faPuzzlePiece }
+                            update={ this.updateHobbies }
+                        />
+                        <div className="form-group row" style={{ marginTop: "10px", textAlign: "center" }}>
+                            <button className="btn btn-primary" onClick={ () => this.generateResume() }>Hit Me With Your Best Shot</button>
+                        </div>
+                    </Form>
                 </div>
             </div>
-            <div className="contact-information">
-                <div className="row">
-                  <div className="col">
-                    <FontAwesomeIcon icon={ faEnvelope } style={{ marginRight: "5px"}}/>
-                    <label>Email Address</label>
-                    <input className="form-control" placeholder="president@uwaterloo.ca" onChange={(e) => this.updateContactInfo({email: e.target.value })} />
-                  </div>
-                  <div className="col">
-                    <FontAwesomeIcon icon={ faPhone } style={{ marginRight: "5px"}}/>
-                    <label>Phone Number</label>
-                    <input className="form-control" placeholder="666-420-6969" onChange={(e) => this.updateContactInfo({phone: e.target.value })} />
-                  </div>
-                  <div className="col">
-                    <FontAwesomeIcon icon={ faFireAlt } style={{ marginRight: "5px"}}/>
-                    <label>Tinder Profile</label>
-                    <input className="form-control" placeholder="looking4truluv" onChange={(e) => this.updateContactInfo({tinder: e.target.value })} />
-                  </div>
-                  <div className="col">
-                    <FontAwesomeIcon icon={ faHashtag } style={{ marginRight: "5px"}}/>
-                    <label>Instagram</label>
-                    <input className="form-control" placeholder="@BarackObama" onChange={(e) => this.updateContactInfo({instagram: e.target.value })} />
-                  </div>
-                </div>
-            </div>
-            <CheckBoxesComponent 
-                title="Languages"
-                fileName="Languages"
-                componentName="languages"
-                icon={ faLanguage }
-                update={ this.updateLanguages }
-            />
-            <CheckBoxesComponent 
-                title="Projects"
-                fileName="Projects"
-                componentName="projects"
-                icon={ faWrench }
-                update={ this.updateProjects }
-            />
-            <CheckBoxesComponent 
-                title="Relevant Work"
-                fileName="RelevantWork"
-                componentName="relevantWork"
-                icon={ faUserTie }
-                update={ this.updateRelevantWork }
-            />
-            <CheckBoxesComponent 
-                title="Hobbies"
-                fileName="Hobbies"
-                componentName="hobbies"
-                icon={ faPuzzlePiece }
-                update={ this.updateHobbies }
-            />
-          <div className="form-group row" style={{ marginTop: "10px", textAlign: "center" }}>
-              <button className="btn btn-primary" onClick={ () => this.generateResume() }>Hit Me With Your Best Shot</button>
-          </div>
-        </Form>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
--- src/pages/PdfContainer.js
+++ src/pages/PdfContainer.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 export default (props) => {
-  const bodyRef = React.createRef();
-  const createPdf = () => props.createPdf(bodyRef.current);
-  return (
-    <section className="pdf-container">
-      <section className="pdf-toolbar">
-        <button onClick={createPdf}>Create PDF</button>
-      </section>
-      <section className="pdf-body" ref={bodyRef}>
-        {props.children}
-      </section>
-    </section>
-  )
+    const bodyRef = React.createRef();
+    const createPdf = () => props.createPdf(bodyRef.current);
+    return (
+        <section className="pdf-container">
+            <section className="pdf-body" ref={bodyRef}>
+                {props.children}
+            </section>
+            <section>
+                <span style={{ backgroundColor: "white" }}>This button doesn't work well.<br />But click for the full, terrible experience anyway.</span>
+            </section>
+            <section className="pdf-toolbar">
+                <button onClick={createPdf}>¯\_(ツ)_/¯</button>
+            </section>
+        </section>
+    )
 }
