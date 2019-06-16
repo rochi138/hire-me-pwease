b183cd0c05bb2a6c3a7f3e6e360c51d90b6691a4
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-06-16 14:49:36 -0400

    Changed buttons

--- src/components/pdf service/PdfContainer.js
+++ src/components/pdf service/PdfContainer.js
@@ -8,11 +8,8 @@ export default (props) => {
             <section class="pdf-body" ref={bodyRef}>
                 {props.children}
             </section>
-            <section>
-                <span style={{ backgroundColor: "white" }}>This button doesn't work well.<br />But click for the full, terrible experience anyway.</span>
-            </section>
             <section class="pdf-toolbar">
-                <button onClick={createPdf}>¯\_(ツ)_/¯</button>
+                <button class="button" onClick={createPdf}>THIS BUTTON DOESN'T WORK WELL.<br />BUT CLICK FOR THE FULL, TERRIBLE EXPERIENCE ANYWAY.<br />¯\_(ツ)_/¯</button>
             </section>
         </section>
     )
