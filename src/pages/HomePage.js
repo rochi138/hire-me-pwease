dc97a91ba63997fe3c2df92f242339a60f365daa
rochi138 <rjching@uwaterloo.ca>
2019-06-17 11:38:44 -0400

    Text background; ContactInfoComponent

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import '../SCSS/styles.css';
-import { CheckBoxesComponent } from '../components/HomePageComponents';
+import { CheckBoxesComponent, ContactInfoComponent } from '../components/HomePageComponents';
 import { Form } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
     faPuzzlePiece,
     faWrench,
     faUserTie,
-    faEnvelope,
-    faPhone,
-    faFireAlt,
-    faHashtag,
     faLanguage
  } from '@fortawesome/free-solid-svg-icons'
 
 export default class Home extends Component{
     constructor(props) {
         super(props);
-
+        
+        this.updateName = this.updateName.bind( this );
+        this.updateContactInfo = this.updateContactInfo.bind( this );
         this.updateHobbies = this.updateHobbies.bind( this );
         this.updateProjects = this.updateProjects.bind( this );
         this.updateRelevantWork = this.updateRelevantWork.bind( this );
@@ -78,75 +75,33 @@ export default class Home extends Component{
                         </div>
                         <div className="container">
                             <Form>
-                                <div className="heading">
-                                    <h2>Personal Information</h2>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <div className="form-group">
-                                            <label>First Name</label>
-                                            <input className="form-control" placeholder="Chad, Stacey, Elon, Obama" onChange={(e) => this.updateName({firstName: e.target.value })} />
-                                        </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="form-group">
-                                            <label >Last Name</label>
-                                            <input className="form-control" placeholder="Smith, Musk, Gates, Jobs, etc." onChange={(e) => this.updateName({lastName: e.target.value })}/>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="contact-information">
-                                    <div className="row">
-                                        <div className="col">
-                                            <FontAwesomeIcon icon={ faEnvelope } style={{ marginRight: "5px"}}/>
-                                            <label>Email Address</label>
-                                            <input className="form-control" placeholder="president@uwaterloo.ca" onChange={(e) => this.updateContactInfo({email: e.target.value })} />
-                                        </div>
-                                        <div className="col">
-                                            <FontAwesomeIcon icon={ faPhone } style={{ marginRight: "5px"}}/>
-                                            <label>Phone Number</label>
-                                            <input className="form-control" placeholder="666-420-6969" onChange={(e) => this.updateContactInfo({phone: e.target.value })} />
-                                        </div>
-                                        <div className="col">
-                                            <FontAwesomeIcon icon={ faFireAlt } style={{ marginRight: "5px"}}/>
-                                            <label>Tinder Profile</label>
-                                            <input className="form-control" placeholder="looking4truluv" onChange={(e) => this.updateContactInfo({tinder: e.target.value })} />
-                                        </div>
-                                        <div className="col">
-                                            <FontAwesomeIcon icon={ faHashtag } style={{ marginRight: "5px"}}/>
-                                            <label>Instagram</label>
-                                            <input className="form-control" placeholder="@BarackObama" onChange={(e) => this.updateContactInfo({instagram: e.target.value })} />
-                                        </div>
-                                    </div>
-                                </div>
+                                <ContactInfoComponent
+                                    updateName={ this.updateName } 
+                                    updateContactInfo={ this.updateContactInfo } />
                                 <CheckBoxesComponent 
                                     title="Languages"
                                     fileName="Languages"
                                     componentName="languages"
                                     icon={ faLanguage }
-                                    update={ this.updateLanguages }
-                                />
+                                    update={ this.updateLanguages } />
                                 <CheckBoxesComponent 
                                     title="Projects"
                                     fileName="Projects"
                                     componentName="projects"
                                     icon={ faWrench }
-                                    update={ this.updateProjects }
-                                />
+                                    update={ this.updateProjects } />
                                 <CheckBoxesComponent 
                                     title="Relevant Work"
                                     fileName="RelevantWork"
                                     componentName="relevantWork"
                                     icon={ faUserTie }
-                                    update={ this.updateRelevantWork }
-                                />
+                                    update={ this.updateRelevantWork } />
                                 <CheckBoxesComponent 
                                     title="Hobbies"
                                     fileName="Hobbies"
                                     componentName="hobbies"
                                     icon={ faPuzzlePiece }
-                                    update={ this.updateHobbies }
-                                />
+                                    update={ this.updateHobbies } />
                                 <div className="form-group row" style={{ marginTop: "10px", textAlign: "center" }}>
                                     <button className="btn btn-primary" onClick={ () => this.generateResume() }>Hit Me With Your Best Shot</button>
                                 </div>
