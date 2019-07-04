57cff427554016f79c245e92ab362a6b453c7fa4
rjching138 <38929552+rjching138@users.noreply.github.com>
2019-07-03 20:38:12 -0400

    Updates

---
+++ precache-manifest.504682c18a007b48b43ee21eb54e22df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7521aebbb624af7524d866d64eb638c",
+    "url": "/hire-me-pwease/static/media/halp.c7521aeb.PNG"
+  },
+  {
+    "revision": "e412fe33c2d7db775410",
+    "url": "/hire-me-pwease/static/css/main.7303dce3.chunk.css"
+  },
+  {
+    "revision": "a6cc22286c65703dab6bfbaecbba2f94",
+    "url": "/hire-me-pwease/static/media/Obama4.a6cc2228.PNG"
+  },
+  {
+    "revision": "22f705f05c2fbea83b18",
+    "url": "/hire-me-pwease/static/js/1.22f705f0.chunk.js"
+  },
+  {
+    "revision": "f2fd77cadb484db280f8",
+    "url": "/hire-me-pwease/static/js/runtime~main.f2fd77ca.js"
+  },
+  {
+    "revision": "f8e98be800aedbcea6a8ada856d5530f",
+    "url": "/hire-me-pwease/static/media/killme.f8e98be8.png"
+  },
+  {
+    "revision": "e412fe33c2d7db775410",
+    "url": "/hire-me-pwease/static/js/main.e412fe33.chunk.js"
+  },
+  {
+    "revision": "317147d79f1702dea05488c56fbb762d",
+    "url": "/hire-me-pwease/static/media/Obama1.317147d7.PNG"
+  },
+  {
+    "revision": "64e3f7189ab0559d9f66911eed4888a5",
+    "url": "/hire-me-pwease/static/media/Obama2.64e3f718.PNG"
+  },
+  {
+    "revision": "4e7d54e8e5b31927236c54dc0e89b712",
+    "url": "/hire-me-pwease/static/media/Obama3.4e7d54e8.PNG"
+  },
+  {
+    "revision": "22f705f05c2fbea83b18",
+    "url": "/hire-me-pwease/static/css/1.109955e0.chunk.css"
+  },
+  {
+    "revision": "ccbbf2d2209c154a64a3a7edd5067dcc",
+    "url": "/hire-me-pwease/index.html"
+  }
+];
