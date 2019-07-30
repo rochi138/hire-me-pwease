816227e19749ae18e43bce1e4d802717939f2208
rochi138 <rjching@uwaterloo.ca>
2019-07-30 11:05:55 -0400

    Edited support labels

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,15 +1,15 @@
 export const TimeOptions= [
     {
-        label: "Miss me with that 'paying' *dabs* <i class='fas fa-hand-middle-finger fa-2x'></i> (3:36)",
-        time: 216
-    },
-    {
-        label: "What I can afford for the developers <i class='fas fa-smile-beam fa-2x'></i> (0:30)",
-        time: 20
-    },
-    {
-        label: "Developers are awesome and deserve compensation for their efforts <i class='fas fa-thumbs-up fa-2x'></i> (0:10)",
+        label: "<i class='fas fa-thumbs-up fa-2x'></i> Developers are awesome and deserve compensation for their efforts (0:10)",
         time: 10
+    },
+    {
+        label: "<i class='fas fa-smile-beam fa-2x'></i> What I can afford for the developers (0:30)",
+        time: 30
+    },
+    {
+        label: "<i class='fas fa-hand-middle-finger fa-2x'></i> Miss me with that 'paying' *dabs* (3:36)",
+        time: 216
     }
 ]
 
